dd5cff70f72bf23f9cb4e5250b40789337cb82ea
Remo Inverardi <remo.inverardi@gmail.com>
2022-03-23 22:35:34 +0100

    ...

--- app/src/main/java/app/egghunt/activity/OrganizerActivity.kt
+++ app/src/main/java/app/egghunt/activity/OrganizerActivity.kt
@@ -3,6 +3,7 @@ package app.egghunt.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import app.egghunt.R
+import app.egghunt.data.Data
 import app.egghunt.databinding.ActivityOrganizerBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,9 +11,11 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DatabaseReference
 
 class OrganizerActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityOrganizerBinding
+    private lateinit var competitionReference: DatabaseReference
     private lateinit var map: GoogleMap
 
     override fun onCreate(state: Bundle?) {
@@ -25,6 +28,13 @@ class OrganizerActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
 
         mapFragment.getMapAsync(this)
+
+        // Sync the competition.
+
+        competitionReference = Data.syncCompetition(
+            intent.extras!!.getString(EXTRA_COMPETITION_DESCRIPTION)!!,
+            intent.extras!!.getString(EXTRA_COMPETITION_TAG)!!
+        )
     }
 
     override fun onMapReady(map: GoogleMap) {
---
+++ app/src/main/java/app/egghunt/data/Data.kt
@@ -0,0 +1,21 @@
+package app.egghunt.data
+
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+object Data {
+    private fun open(): FirebaseDatabase {
+        val url = "https://egg-hunt-344616-default-rtdb.europe-west1.firebasedatabase.app"
+
+        return FirebaseDatabase.getInstance(url)
+    }
+
+    fun syncCompetition(competitionDescription: String, competitionTag: String): DatabaseReference {
+        val reference = open().getReference("competition/$competitionTag")
+
+        reference.child("description").setValue(competitionDescription)
+        reference.keepSynced(true)
+
+        return reference
+    }
+}
