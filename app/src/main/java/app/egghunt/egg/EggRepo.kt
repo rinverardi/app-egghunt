c3d05f979c74054e95ee1b374e5c26cc6803abb7
Remo Inverardi <remo@inverardi.ch>
2022-04-15 23:28:02 +0200

    Clean up.

--- app/src/main/java/app/egghunt/egg/EggRepo.kt
+++ app/src/main/java/app/egghunt/egg/EggRepo.kt
@@ -5,14 +5,13 @@ import androidx.recyclerview.widget.RecyclerView
 import app.egghunt.lib.Keys
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.Query
 
 object EggRepo {
     fun bind(competition: DatabaseReference, recycler: RecyclerView): EggAdapter {
-        val query = competition.child(Keys.EGG).orderByChild(Keys.DESCRIPTION)
-
         val adapter = EggAdapter(
             FirebaseRecyclerOptions.Builder<Egg>()
-                .setQuery(query, Egg::class.java)
+                .setQuery(makeQuery(competition), Egg::class.java)
                 .build()
         )
 
@@ -23,4 +22,7 @@ object EggRepo {
 
         return adapter
     }
+
+    private fun makeQuery(competition: DatabaseReference): Query =
+        competition.child(Keys.EGG).orderByChild(Keys.DESCRIPTION)
 }
--- app/src/main/java/app/egghunt/hint/HintRepo.kt
+++ app/src/main/java/app/egghunt/hint/HintRepo.kt
@@ -5,14 +5,13 @@ import androidx.recyclerview.widget.RecyclerView
 import app.egghunt.lib.Keys
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.Query
 
 object HintRepo {
     fun bind(competition: DatabaseReference, recycler: RecyclerView): HintAdapter {
-        val query = competition.child(Keys.HINT).orderByChild(Keys.ORDER)
-
         val adapter = HintAdapter(
             FirebaseRecyclerOptions.Builder<Hint>()
-                .setQuery(query, Hint::class.java)
+                .setQuery(makeQuery(competition), Hint::class.java)
                 .build()
         )
 
@@ -23,4 +22,7 @@ object HintRepo {
 
         return adapter
     }
+
+    private fun makeQuery(competition: DatabaseReference): Query =
+        competition.child(Keys.HINT).orderByChild(Keys.ORDER)
 }
